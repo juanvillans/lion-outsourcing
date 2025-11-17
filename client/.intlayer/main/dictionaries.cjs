d96dd710af049d0e264975f7b69e2a9c1249bb58
juanvillans <juanvillans16@gmail.com>
2025-11-17 17:11:43 -0400

    deleting client

--- client/.intlayer/main/dictionaries.cjs
+++
@@ -1,8 +0,0 @@
-
-const dictionaries = {
-
-};
-const getDictionaries = () => dictionaries;
-
-module.exports.getDictionaries = getDictionaries;
-module.exports = dictionaries;
--- client/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-import {withIntlayer} from "next-intlayer/server";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default withIntlayer(nextConfig);
