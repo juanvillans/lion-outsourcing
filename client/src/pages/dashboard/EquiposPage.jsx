dc1b7521b21bc565db88fd8fbb5e2f6f2baaf99c
juandonquis <juandonquis07@gmail.com>
2026-01-12 10:43:32 -0400

    ni

--- client/src/pages/dashboard/EquiposPage.jsx
+++ client/src/pages/dashboard/EquiposPage.jsx
@@ -16,23 +16,26 @@ export default function EquiposPage() {
           </FuturisticButton>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 lg:gap-10">
-          <div className="relative group cursor-pointer hover:text-gray-400 equipoFolder">
-            <Icon
-              icon="fa6-solid:folder"
-              className="text-gray-200 icon group-hover:text-gray-200 absolute w-full h-full cursor-pointer"
-            />
-            <div className="absolute w-full h-full top-10 left-0">
-              <h2>Equipo 1</h2>
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 lg:gap-10 mt-10">
+          <div className="p-4 relative neuphormism bg-gray-100 group cursor-pointer hover:text-gray-700 equipoFolder">
+              <h2 className="font-semibold mb-2">Equipo 1</h2>
+
+              <div className="flex gap-2">
+                <Icon icon="fluent:text-description-16-filled" className="w-16 text-gray-500 mt-1" />
+                <p className="text-sm text-gray-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero esse maiores a quo enim deserunt. Ipsa minima assumenda adipisci officiis debitis, quo nam fugiat eum, iste enim dolores laudantium vitae?</p>
+              </div>
           </div>
           
-          <div className="relative">
-            <Icon
-              icon="fa6-solid:folder"
-              className="text-gray-200 w-full h-full cursor-pointer hover:text-gray-400"
-            />
+          <div className="p-4 relative neuphormism bg-gray-100 group cursor-pointer hover:text-gray-400 equipoFolder">
+              <h2 className="font-semibold">Equipo 1</h2>
+              <p></p>
           </div>
+
+          <div className="p-4 relative neuphormism bg-gray-100 group cursor-pointer hover:text-gray-400 equipoFolder">
+              <h2 className="font-semibold">Equipo 1</h2>
+              <p></p>
+          </div>
+
         </div>
       </div>
     </div>
